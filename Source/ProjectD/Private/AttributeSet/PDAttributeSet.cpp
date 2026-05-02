15e7a8a3d6398514f13e119bb3bb146377c0fd39
hamster1198 <sk65771476@gmail.com>
2026-05-02 23:12:56 +0900

    [ADD] AttributeSet

---
+++ Source/ProjectD/Private/AttributeSet/PDAttributeSet.cpp
@@ -0,0 +1,123 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttributeSet/PDAttributeSet.h"
+#include "GameplayEffectExtension.h"
+#include "Characters/PDCharacterBase.h"
+#include "Data/PDBodyPartConfig.h"
+
+void UPDAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+	HandleAttributeClamp(Attribute, NewValue);
+}
+
+void UPDAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		const float LocalDamage = GetDamage();
+		SetDamage(0.f);
+
+		if (LocalDamage <= 0.f) return;
+		
+		EBodyPart Part = EBodyPart::Torso;
+		if (const FHitResult* HitResult = Data.EffectSpec.GetContext().GetHitResult())
+		{//HitResult´Â EffectContext ¾È¿¡
+			if (BodyPartConfig)
+			{
+				Part = BodyPartConfig->GetBodyPartFromName(HitResult->BoneName);
+			}
+		}
+		const float NewHP = FMath::Max(GetHPByPart(Part) - LocalDamage, 0.f);
+		SetHPByPart(Part, NewHP);
+		
+		if (GetTorsoHP() <= 0.f || GetHeadHP() <= 0.f)
+		{
+			if (APDCharacterBase* Owner = Cast<APDCharacterBase>(GetOwningActor()))
+			{
+				Owner->HandleDeath(Data.EffectSpec.GetContext().GetInstigator());
+			}
+		}
+	}
+}
+
+void UPDAttributeSet::HandleAttributeClamp(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	NewValue=FMath::Max(NewValue, 0.f);
+
+	if (Attribute==GetHeadHPAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxHeadHP());
+	else if (Attribute==GetTorsoHPAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxTorsoHP());
+	else if (Attribute==GetArmLHPAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxArmLHP());
+	else if (Attribute==GetArmRHPAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxArmRHP());
+	else if (Attribute==GetLegLHPAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxLegLHP());
+	else if (Attribute==GetLegRHPAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxLegRHP());
+	else if (Attribute==GetStaminaAttribute()) NewValue=FMath::Clamp(NewValue, 0.f, GetMaxStamina());
+}
+
+FGameplayAttribute UPDAttributeSet::GetAttributeByPart(EBodyPart Part) const
+{
+	switch (Part)
+	{
+	case EBodyPart::Head:  return GetHeadHPAttribute();
+	case EBodyPart::Arm_L: return GetArmLHPAttribute();
+	case EBodyPart::Arm_R: return GetArmRHPAttribute();
+	case EBodyPart::Leg_L: return GetLegLHPAttribute();
+	case EBodyPart::Leg_R: return GetLegRHPAttribute();
+	default:               return GetTorsoHPAttribute();
+	}
+}
+void UPDAttributeSet::ApplyDamageToPart(EBodyPart Part, float DamageAmount)
+{
+	switch (Part)
+	{
+	case EBodyPart::Head:
+		SetHeadHP(FMath::Max(GetHeadHP() - DamageAmount, 0.f));
+		break;
+	case EBodyPart::Arm_L:
+		SetArmLHP(FMath::Max(GetArmLHP() - DamageAmount, 0.f));
+		break;
+	case EBodyPart::Arm_R:
+		SetArmRHP(FMath::Max(GetArmRHP() - DamageAmount, 0.f));
+		break;
+	case EBodyPart::Leg_L:
+		SetLegLHP(FMath::Max(GetLegLHP() - DamageAmount, 0.f));
+		break;
+	case EBodyPart::Leg_R:
+		SetLegRHP(FMath::Max(GetLegRHP() - DamageAmount, 0.f));
+		break;
+	case EBodyPart::Torso:
+	default:
+		SetTorsoHP(FMath::Max(GetTorsoHP() - DamageAmount, 0.f));
+		break;
+	}
+}
+
+float UPDAttributeSet::GetHPByPart(EBodyPart Part) const
+{
+	switch (Part)
+	{
+		case EBodyPart::Head:  return GetHeadHP();
+		case EBodyPart::Torso: return GetTorsoHP();
+		case EBodyPart::Arm_L: return GetArmLHP();
+		case EBodyPart::Arm_R: return GetArmRHP();
+		case EBodyPart::Leg_L: return GetLegLHP();
+		case EBodyPart::Leg_R: return GetLegRHP();
+		default:               return 0.f;
+	}
+}
+
+void UPDAttributeSet::SetHPByPart(EBodyPart Part, float NewValue)
+{
+	switch (Part)
+	{
+		case EBodyPart::Head:  SetHeadHP(NewValue);  break;
+		case EBodyPart::Torso: SetTorsoHP(NewValue); break;
+		case EBodyPart::Arm_L: SetArmLHP(NewValue);  break;
+		case EBodyPart::Arm_R: SetArmRHP(NewValue);  break;
+		case EBodyPart::Leg_L: SetLegLHP(NewValue);  break;
+		case EBodyPart::Leg_R: SetLegRHP(NewValue);  break;
+	}
+}
---
+++ Source/ProjectD/Public/AttributeSet/PDAttributeSet.h
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Type/Types.h"
+#include "PDAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+class UPDBodyPartConfig;
+class APDCharacterBase;
+
+UCLASS()
+class PROJECTD_API UPDAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData HeadHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, HeadHP)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData MaxHeadHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxHeadHP)
+
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData TorsoHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, TorsoHP)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData MaxTorsoHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxTorsoHP)
+
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData ArmLHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, ArmLHP)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData MaxArmLHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxArmLHP)
+
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData ArmRHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, ArmRHP)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData MaxArmRHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxArmRHP)
+
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData LegLHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, LegLHP)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData MaxLegLHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxLegLHP)
+
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData LegRHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, LegRHP)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Health")
+	FGameplayAttributeData MaxLegRHP;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxLegRHP)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Stamina")
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, Stamina)
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Stamina")
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, MaxStamina)
+	
+	//Meta Attribute
+	UPROPERTY(BlueprintReadOnly, Category = "PD|Meta")
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UPDAttributeSet, Damage)
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UPDBodyPartConfig> BodyPartConfig;
+protected:
+	//Pre -> Clamping 
+	//Post -> GE (Death, Apply Damage)
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+private:
+	void HandleAttributeClamp(const FGameplayAttribute& Attribute, float& NewValue) const;
+	void ApplyDamageToPart(EBodyPart Part, float DamageAmount);
+	
+	float GetHPByPart(EBodyPart Part) const;
+	void SetHPByPart(EBodyPart Part, float NewValue);
+	
+	FGameplayAttribute GetAttributeByPart(EBodyPart Part) const;
+	
+	
+};
