7ec565c4bd0abf6fa7f26528cc2eefa88f5ba0aa
hamster1198 <sk65771476@gmail.com>
2026-05-03 14:56:11 +0900

    [UPD] Raid GameMode

--- Source/ProjectD/Public/Core/PDGameMode.h
+++ Source/ProjectD/Public/Core/PDGameMode.h
@@ -2,7 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Type/Types.h"
 #include "PDGameMode.generated.h"
+
+class APDPlayerController;
 
 UCLASS(abstract)
 class PROJECTD_API APDGameMode : public AGameModeBase
@@ -11,4 +14,27 @@ class PROJECTD_API APDGameMode : public AGameModeBase
 
 public:
 	APDGameMode();
+	
+	UFUNCTION(BlueprintCallable, Category="PD|Raid")
+	void StartRaid();
+	
+	UFUNCTION(BlueprintCallable, Category="PD|Raid")
+	void RequestExtraction(APlayerController* PC);
+	
+	UFUNCTION(BlueprintCallable, Category="PD|Raid")
+	void EndRaid(bool bSuccess);
+	
+	void OnPlayerDied(APlayerController* PC, AActor* Killer);
+	FORCEINLINE ERaidState GetRaidState() { return CurrentRaidState; }
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="PD|Raid")
+	ERaidState CurrentRaidState=ERaidState::Idle;
+	
+	void SetRaidState(ERaidState NewState);
+	
+	UFUNCTION(BlueprintImplementableEvent, Category="PD|Raid")
+	void OnRaidStateChanged(ERaidState NewState);
+	
+	
 };
