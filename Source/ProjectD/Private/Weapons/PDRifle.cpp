dd830b9244d3a682d6133f56afac9af33d290e1a
이경환\lkh12 <lkh12251011@gmail.com>
2026-05-06 20:06:41 +0900

    [Add] 누락된 라이플 헤더 cpp 추가

---
+++ Source/ProjectD/Private/Weapons/PDRifle.cpp
@@ -0,0 +1,148 @@
+﻿#include "Weapons/PDRifle.h"
+#include "Core/PDPlayerController.h"
+#include "DrawDebugHelpers.h"
+
+APDRifle::APDRifle()
+{
+    WeaponType = EWeaponType::Rifle;
+
+    DefaultFOV = 90.f;
+    ZoomedFOV = 70.f;
+
+    LevelStats.Add({ 20.f, 0.12f, 1500.f, 30, 2.0f, 0.90f }); // Lv1
+    LevelStats.Add({ 28.f, 0.10f, 1700.f, 35, 1.8f, 0.93f }); // Lv2
+    LevelStats.Add({ 38.f, 0.08f, 2000.f, 40, 1.5f, 0.96f }); // Lv3
+}
+
+void APDRifle::Fire_Implementation()
+{
+    if (!CanFire()) return;
+
+    FHitResult Hit;
+    if (PerformLineTrace(Hit))
+        ApplyDamage(Hit.GetActor(), GetCurrentStats().Damage);
+
+    PostFire();
+
+    FTimerHandle T_Shell;
+    GetWorldTimerManager().SetTimer(T_Shell, FTimerDelegate::CreateLambda([this]()
+        {
+            EjectShell();
+        }), 0.05f, false);
+
+    if (CurrentAmmo <= 0) StopFire();
+}
+
+void APDRifle::Reload_Implementation()
+{
+    if (bIsReloading) return;
+    if (CurrentAmmo >= GetCurrentStats().MaxAmmo) return;
+
+    bIsReloading = true;
+
+    if (ReloadMontage)
+    {
+        PlayWeaponMontage(ReloadMontage);
+        BindMontageEndedForReload(ReloadMontage);
+    }
+    else
+    {
+        // 몽타주 미설정 시 폴백
+        GetWorldTimerManager().SetTimer(
+            ReloadHandle, this,
+            &APDWeaponBase::FinishReload,
+            GetCurrentStats().ReloadTime, false);
+    }
+}
+
+void APDRifle::StartFire()
+{
+    if (bIsFiring) return;
+    bIsFiring = true;
+
+    Fire();
+
+    if (FireMode == EFireMode::Auto)
+    {
+        GetWorldTimerManager().SetTimer(
+            AutoFireHandle, this,
+            &APDRifle::Fire_Implementation,
+            GetCurrentStats().FireRate, true);
+    }
+}
+
+void APDRifle::StopFire()
+{
+    bIsFiring = false;
+    GetWorldTimerManager().ClearTimer(AutoFireHandle);
+}
+
+void APDRifle::ToggleFireMode()
+{
+    FireMode = (FireMode == EFireMode::Auto)
+        ? EFireMode::Single : EFireMode::Auto;
+
+    GetWorldTimerManager().ClearTimer(AutoFireHandle);
+    OnFireModeChanged.Broadcast(FireMode);
+}
+
+void APDRifle::OnUnequip_Implementation()
+{
+    StopFire();
+    Super::OnUnequip_Implementation();
+}
+
+bool APDRifle::PerformLineTrace(FHitResult& OutHit)
+{
+    const FWeaponLevelStats& Stats = GetCurrentStats();
+
+    AActor* WeaponOwnerActor = GetWeaponOwner();
+    if (!WeaponOwnerActor) return false;
+
+    APlayerController* PC = Cast<APlayerController>(
+        WeaponOwnerActor->GetInstigatorController());
+
+    // 머즐 소켓 있으면 소켓 위치, 없으면 플레이어 위치
+    FVector Start = WeaponMesh->DoesSocketExist(MuzzleSocketName)
+        ? WeaponMesh->GetSocketLocation(MuzzleSocketName)
+        : WeaponOwnerActor->GetActorLocation();
+
+    FVector Forward = WeaponOwnerActor->GetActorForwardVector();
+
+    if (PC)
+    {
+        FHitResult CursorHit;
+        if (PC->GetHitResultUnderCursor(ECC_Visibility, true, CursorHit))
+        {
+            FVector Dir = CursorHit.Location - Start; // Start 기준으로 방향 계산
+            Dir.Z = 0.f;
+            if (!Dir.IsNearlyZero())
+                Forward = Dir.GetSafeNormal();
+        }
+    }
+
+    float SpreadAngle = (1.f - Stats.Accuracy) * 15.f;
+    FVector ShootDir = FMath::VRandCone(Forward, FMath::DegreesToRadians(SpreadAngle));
+    ShootDir.Z = 0.f;
+    ShootDir.Normalize();
+
+    FVector End = Start + ShootDir * Stats.Range;
+
+    FCollisionQueryParams Params;
+    Params.AddIgnoredActor(this);
+    Params.AddIgnoredActor(WeaponOwnerActor);
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Pawn, Params);
+
+    if (bHit)
+    {
+        DrawDebugLine(GetWorld(), Start, OutHit.Location, FColor::Red, false, 1.f, 0, 1.f);
+        DrawDebugSphere(GetWorld(), OutHit.Location, 10.f, 8, FColor::Red, false, 1.f);
+    }
+    else
+    {
+        DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1.f, 0, 1.f);
+    }
+
+    return bHit;
+}
---
+++ Source/ProjectD/Public/Weapons/PDRifle.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/PDWeaponBase.h"
+#include "PDRifle.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnFireModeChanged, EFireMode, NewFireMode);
+
+UCLASS(Blueprintable)
+class PROJECTD_API APDRifle : public APDWeaponBase
+{
+	GENERATED_BODY()
+
+public:
+    APDRifle();
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PD|Weapon|Rifle")
+    EFireMode FireMode = EFireMode::Auto;
+
+    FTimerHandle AutoFireHandle;
+    bool bIsFiring = false;
+
+public:
+    UPROPERTY(BlueprintAssignable, Category = "PD|Weapon|Events")
+    FOnFireModeChanged OnFireModeChanged;
+
+public:
+    virtual void Fire_Implementation() override;
+    virtual void Reload_Implementation() override;
+    virtual void OnUnequip_Implementation() override;
+
+    UFUNCTION(BlueprintCallable, Category = "PD|Weapon|Rifle")
+    void StartFire();
+
+    UFUNCTION(BlueprintCallable, Category = "PD|Weapon|Rifle")
+    void StopFire();
+
+    UFUNCTION(BlueprintCallable, Category = "PD|Weapon|Rifle")
+    void ToggleFireMode();
+
+    UFUNCTION(BlueprintPure, Category = "PD|Weapon|Rifle")
+    FORCEINLINE EFireMode GetFireMode() const { return FireMode; }
+
+private:
+    bool PerformLineTrace(FHitResult& OutHit);
+};
