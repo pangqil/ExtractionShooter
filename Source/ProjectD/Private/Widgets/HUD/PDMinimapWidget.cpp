98aa085e56f00c55d1de161be5fa7bef768f57cd
KYMMMM01 <dbals498@gmail.com>
2026-05-07 22:02:44 +0900

    [FEAT] PDMinimapWidget

--- Source/ProjectD/Private/Widgets/HUD/PDMinimapWidget.cpp
+++ Source/ProjectD/Private/Widgets/HUD/PDMinimapWidget.cpp
@@ -1,5 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Widgets/HUD/PDMinimapWidget.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/Pawn.h"
 
+void UPDMinimapWidget::NativeTick(const FGeometry& Geo, float DeltaTime)
+{
+	Super::NativeTick(Geo, DeltaTime);
+
+	if (APlayerController* PC = GetOwningPlayer())
+	{
+		if (APawn* Pawn = PC->GetPawn())
+		{
+			const FVector Loc = Pawn->GetActorLocation();
+			const float Yaw = Pawn->GetActorRotation().Yaw;
+			OnPlayerTransformUpdated(Loc, Yaw);
+		}
+	}
+}
--- Source/ProjectD/Public/Widgets/HUD/PDMinimapWidget.h
+++ Source/ProjectD/Public/Widgets/HUD/PDMinimapWidget.h
@@ -1,17 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Widgets/PDActivatableBase.h"
 #include "PDMinimapWidget.generated.h"
 
-/**
- * 
- */
-UCLASS()
+UCLASS(Abstract, Blueprintable)
 class PROJECTD_API UPDMinimapWidget : public UPDActivatableBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	//미니맵 표시 반경 (월드 단위, cm)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Minimap")
+	float MapRadius = 2000.f;
+
+protected:
+	virtual void NativeTick(const FGeometry& Geo, float DeltaTime) override;
+
+	//BP에서 갱신 (플레이어 위치/회전)
+	UFUNCTION(BlueprintImplementableEvent, Category="Minimap")
+	void OnPlayerTransformUpdated(const FVector& Location, float Yaw);
 };
