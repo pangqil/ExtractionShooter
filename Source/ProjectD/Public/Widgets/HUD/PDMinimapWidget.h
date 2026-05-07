ca695ac6820e561518241f4b4d3ce75a8467c634
KYMMMM01 <dbals498@gmail.com>
2026-05-07 21:22:34 +0900

    [ADD] PDMinimapWidget 생성

---
+++ Source/ProjectD/Public/Widgets/HUD/PDMinimapWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/PDActivatableBase.h"
+#include "PDMinimapWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTD_API UPDMinimapWidget : public UPDActivatableBase
+{
+	GENERATED_BODY()
+	
+};
