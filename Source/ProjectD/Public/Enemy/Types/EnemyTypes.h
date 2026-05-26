5da613f5e0f6850835800c06286ed6aa0e9e0659
LCW <cksdn43@cau.ac.kr>
2026-05-26 12:59:45 +0900

    [FIX] LootBox Bug

--- Source/ProjectD/Public/Enemy/Types/EnemyTypes.h
+++ Source/ProjectD/Public/Enemy/Types/EnemyTypes.h
@@ -41,20 +41,20 @@ struct FPDLootEntry
 	GENERATED_BODY()
 
 	/** 스폰할 아이템 액터 클래스. APDLootItem 자손. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PD|Loot")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PD|Loot")
 	TSubclassOf<APDLootItem> ItemClass;
 
 	/** DT_ItemData 의 ItemID 컬럼과 매칭. None 이면 ItemClass BP 디폴트의 ItemRowName 사용. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PD|Loot")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PD|Loot")
 	FName ItemID;
 
 	/** 0~1. 0 이면 항상 미드랍, 1 이면 항상 드랍. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PD|Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PD|Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
 	float DropChance = 1.0f;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PD|Loot", meta = (ClampMin = "1"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PD|Loot", meta = (ClampMin = "1"))
 	int32 MinQuantity = 1;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PD|Loot", meta = (ClampMin = "1"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PD|Loot", meta = (ClampMin = "1"))
 	int32 MaxQuantity = 1;
 };
